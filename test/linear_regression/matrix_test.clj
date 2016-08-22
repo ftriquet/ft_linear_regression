d9cf8fa7f0b609b9d96eb49e3bc83a6a2c4c2b5e
François Triquet <francois.triquet@recast.ai>
2016-08-22 23:16:21 +0200

    more tests for matrix multiplication

--- test/linear_regression/matrix_test.clj
+++ test/linear_regression/matrix_test.clj
@@ -3,7 +3,19 @@
             [linear-regression.matrix :refer :all]))
 
 (deftest mult-test
-  (is (= 4 (mult 2 2)))
+  (is (= 4 (mult 2 2)) "Numbers")
+
+  (is (= [[6 10]
+          [8 12]]
+         (mult 2 [[3 5] [4 6]])) "Number and matrix")
+
+  (is (= [[6 10]
+          [8 12]]
+         (mult [[3 5] [4 6]] 2)) "Matrix and number")
+
+  (is (= [2 4 6 8 10] (mult 2 [1 2 3 4 5])) "Number and line vector")
+
+  (is (= [[2] [4] [6] [8] [10]] (mult 2 [[1] [2] [3] [4] [5]])) "Number and column vector")
 
   (is (= [[4 4 4]
           [4 4 4]
@@ -22,11 +34,11 @@
                 [4 5 6]
                 [7 8 9]])))
 
-  (is (= (mult [[1 2]] [[1 100] [1 100]]) [[3 300]]))
- ; [1 2] should be interpreted the same way than [[1 2]]
-  (is (= (mult [1 2] [[1 100] [1 100]]) [[3 300]]))
+  ;[1 2] should be interpreted the same way than [[1 2]]
+  (is (= (mult [[1 2]] [[1 100] [1 100]]) [[3 300]]) "Line vector as a matrix")
+  (is (= (mult [1 2] [[1 100] [1 100]]) [[3 300]]) "Line vector as a vector")
 
-  (is (= (mult [[1] [2]]
-               [[1 100]]) [[1 100]
-                           [2 200]]))
+  (is (= (mult [[1] [2]] [[1 100]])
+         [[1 100]
+          [2 200]]) "Column vector")
   )
