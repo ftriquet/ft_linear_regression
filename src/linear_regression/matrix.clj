de262966454629527d24976748e179b9fd987636
François Triquet <francois.triquet@recast.ai>
2016-08-24 21:56:41 +0200

    Update matrix calculus

--- src/linear_regression/matrix.clj
+++ src/linear_regression/matrix.clj
@@ -1,12 +1,31 @@
 (ns linear-regression.matrix)
 
-(defn transpose [m]
-  (apply map list m))
+(defn l-vec? [m]
+  (and (coll? m) (not (coll? (first m)))))
 
-(defn v->m [m]
-  (if (coll? (first m))
-    m
-    (list m)) )
+(defn c-vec? [m]
+  (if (not (number? m))
+    (let [f (first m)]
+      (and (coll? f) (= 1 (count f))))
+    false))
+
+(defn matrix? [m]
+  (and (not (number? m)) (not (l-vec? m))))
+
+
+(defn transpose [m]
+  (cond
+    (number? m) m
+    (c-vec? m) (flatten m)
+    (l-vec? m) (map list m)
+    :else (apply map list m)))
+
+(defn dims [m]
+  (cond
+    (number? m) 0
+    (l-vec? m) (list 1 (count m))
+    :else (list (count m) (count (first m)))))
+
 
 (defn mult [m1 m2]
   (cond
@@ -15,11 +34,18 @@
                    (for [line m2]
                      (mult m1 line)))
     (number? m2) (mult m2 m1)
-    :else (let [mm1 (v->m m1)
-                mm2 (v->m m2)
-                raw-mult (for [line mm1
-                               col  (transpose mm2)]
-                           (reduce + (map * line col)))]
-            (if (= (count mm1) 1) ;if m1 is a line vector partition will not do what we want
-              (list raw-mult)
-              (partition (count mm1) raw-mult)))))
+    (not= (first (dims m2)) (second (dims m1))) (throw (IllegalArgumentException.
+                                                         "Invalid matrix sizes"))
+    (l-vec? m1) (if (c-vec? m2)
+                  (reduce + (map * m1 (transpose m2)))
+                  (for [col (transpose m2)]
+                    (mult m1 (transpose (reverse col)))))
+    (c-vec? m1) (if (l-vec? m2)
+                  (for [line m1]
+                    (mult (first line) m2))
+                  (let [tr (transpose m1)]
+                    (for [i m2]
+                      (transpose (map #(* i %) tr)))))
+    :else (partition (count m1)
+                     (for [line m1 col (transpose m2)]
+                       (reduce + (map * line col))))))
--- test/linear_regression/matrix_test.clj
+++ test/linear_regression/matrix_test.clj
@@ -2,6 +2,32 @@
   (:require [clojure.test :refer :all]
             [linear-regression.matrix :refer :all]))
 
+(deftest l-vec?-test
+  (is (l-vec? [1 2 3 4]) "Line vector")
+  (is (= false (l-vec? 1)) "Number")
+  (is (= false (l-vec? [[1] [2]])) "Column vector")
+  (is (= false (l-vec? [[1 2 3] [4 5 6] [7 8 9]])) "Matrix"))
+
+(deftest c-vec?-test
+
+  (is (= false (c-vec? [1 2 3 4])) "Line vector")
+  (is (= false (c-vec? 1)) "Number")
+  (is (= true  (c-vec? [[1] [2]])) "Column vector")
+  (is (= false (c-vec? [[1 2 3] [4 5 6] [7 8 9]])) "Matrix"))
+
+
+(deftest matrix?-test
+  (is (= true (matrix? [[1 4] [2 5] [3 6]])) "Matrix")
+  (is (= false (matrix? 8)) "Number")
+  (is (= false (matrix? [1 2 3 4])) "Vector")
+  (is (= true (matrix? [[1] [2] [3]])) "Column vector"))
+
+(deftest dims-test
+  (is (= '(1 2) (dims [4 5])))
+  (is (= '(3 4) (dims [[1 2 3 4] [1 2 3 4] [1 2 3 4]])))
+  )
+
+
 (deftest mult-test
   (is (= 4 (mult 2 2)) "Numbers")
 
@@ -17,13 +43,6 @@
 
   (is (= [[2] [4] [6] [8] [10]] (mult 2 [[1] [2] [3] [4] [5]])) "Number and column vector")
 
-  (is (= [[4 4 4]
-          [4 4 4]
-          [4 4 4]]
-         (mult 4 [[1 1 1]
-                  [1 1 1]
-                  [1 1 1]])))
-
   (is (= [[30 36 42]
           [66 81 96]
           [102 126 150]]
@@ -32,13 +51,26 @@
                 [7 8 9]]
                [[1 2 3]
                 [4 5 6]
-                [7 8 9]])))
+                [7 8 9]])) "Two square matrices")
 
-  ;[1 2] should be interpreted the same way than [[1 2]]
-  (is (= (mult [[1 2]] [[1 100] [1 100]]) [[3 300]]) "Line vector as a matrix")
-  (is (= (mult [1 2] [[1 100] [1 100]]) [[3 300]]) "Line vector as a vector")
-
-  (is (= (mult [[1] [2]] [[1 100]])
+  (is (= (mult [1 2] [[1 100] [1 100]]) [3 300]) "Line by matrix")
+  (is (= 30 (mult [1 2 3 4] [[1] [2] [3] [4]])) "Line by column")
+  (is (= (mult [[1] [2]] [1 100])
          [[1 100]
-          [2 200]]) "Column vector")
+          [2 200]]) "Column by line")
+
   )
+
+(deftest transpose-test
+  (is (= (transpose [[1 2 3 4]]) [[1] [2] [3] [4]]) "Line vector")
+  (is (= (transpose [[1 2 3]
+                     [4 5 6]]) [[1 4]
+                                [2 5]
+                                [3 6]]) "Matrix")
+  (is (= (transpose [[1] [2] [3] [4]]) [1 2 3 4]) "Column vector"))
+
+(comment
+
+  (deftest predict-test
+    (is (= 60 (predict [[1 3 2 5]] [[3 5 8]])))
+    ))
