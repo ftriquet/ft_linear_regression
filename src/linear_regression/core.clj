bd4059508531b07f71e2480119a230b5c6223e5c
François Triquet <francois.triquet@recast.ai>
2016-08-27 21:28:44 +0200

    draw line after training

--- src/linear_regression/core.clj
+++ src/linear_regression/core.clj
@@ -2,6 +2,8 @@
   (:gen-class)
   (:require [linear-regression.matrix :refer :all]
             [linear-regression.learning :refer :all]
+            [quil.core :as q]
+            [quil.middleware :as m]
             [clojure-csv.core :as csv]))
 
 (defn- check-data [data]
@@ -26,7 +28,15 @@
         (if (check-data data)
           (let [[inputs labels] (format-data data)
                 results (train-model inputs labels 0.1)]
-            (->> results
+            (->> (:csv-data results)
                  (csv/write-csv)
-                 (spit "resources/thetas.csv")))
+                 (spit "resources/thetas.csv"))
+            (when (= 2 (cols (:X results)))
+              (q/defsketch linear-regression
+                :title "Linear Regression"
+                :size [800 800]
+                :setup (init-draw results)
+                :draw draw-result
+                :update upd
+                :middleware [m/fun-mode])))
           (println "Invalid csv file"))))))
--- src/linear_regression/learning.clj
+++ src/linear_regression/learning.clj
@@ -1,6 +1,5 @@
 (ns linear-regression.learning
   (:require [linear-regression.matrix :refer :all]
-            [quil.middleware :as m]
             [quil.core :as q]))
 
 (defn sq [x]
@@ -52,11 +51,39 @@
       (list (list (apply min tr)) (list (apply max tr)))
       (list (map #(apply min %) tr) (map #(apply max %) tr)))))
 
+(defn init-draw [{X :X theta :theta labels :labels minx :min maxx :max :as state}]
+  (fn []
+    (q/frame-rate 1)
+    (q/background 240)
+    {:X X
+     :min minx
+     :max maxx
+     :theta theta
+     :labels labels}))
+
+(defn upd [state] state)
+
+(defn draw-result [{X :X theta :theta labels :labels minx :min maxx :max :as state}]
+  (q/background 255)
+  (doseq [[x [y]] (partition 2 (interleave (map second X) labels))]
+    (q/rect (* 800.0 x)
+            (- 800 (* 800.0 (/ (double y) (apply max (map first labels)))))
+            4 4))
+  (let [x1 0.0
+        y1 (+ (first theta) (* x1 (second theta)))
+        x2 1.0
+        y2 (+ (first theta) (* x2 (second theta)))]
+    (q/line
+      (* 800.0 x1)
+      (- 800 (* 800.0 (/ (double y1) (apply max (map first labels)))))
+      (* 800.0 x2)
+      (- 800 (* 800.0 (/ (double y2) (apply max (map first labels))))))))
+
 
 (defn train-model [inputs labels lrn-rate]
   (let [X (add-bias-feature (scale inputs))
         weights (zeros (inc (cols inputs)))
-       [minsX maxsX] (get-ext inputs)]
+        [minsX maxsX] (get-ext inputs)]
     (println "Start computing, actual thetas: " weights)
     (loop [theta weights
            error (cost X theta labels sq)]
@@ -64,9 +91,14 @@
                           (mult (to-line (compute-costs X theta labels)) X))
             new-theta (sub theta d-theta)
             err (cost X new-theta labels sq)]
-        (if (< (- error err) 0.00001)
+        (if (< (- error err) 0.000001)
           (do
             (println "Done! Thetas: " new-theta)
-            [["mins" "maxs" "theta"]
-             [(pr-str minsX) (pr-str maxsX) (pr-str new-theta)]])
+            {:csv-data [["mins" "maxs" "theta"]
+                        [(pr-str minsX) (pr-str maxsX) (pr-str new-theta)]]
+             :X X
+             :theta new-theta
+             :labels labels
+             :min minsX
+             :max maxsX})
           (recur new-theta err))))))
